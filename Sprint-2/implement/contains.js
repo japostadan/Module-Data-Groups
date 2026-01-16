8ee165d2b73da5fd0aa81d49a8a53da106e24bfb
japostad <japostad@student.42barcelona.com>
2026-01-16 22:01:31 +0100

    Mod: finished coursework

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,13 @@
-function contains() {}
+function contains(obj, key) {
+  if (typeof (obj !== "obj")) throw new TypeError("Invalid object parameter");
+
+  for (prop in obj) {
+    prop = key;
+    if (obj.hasOwnProperty(prop)) {
+      return true;
+    }
+  }
+  return false;
+}
 
 module.exports = contains;
